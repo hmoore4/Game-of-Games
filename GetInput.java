b18a28119fe9b166327aca0eb8cbb87ae650fce7
Hunter <98huntermoore@gmail.com>
2020-11-14 21:16:19 -0500

    Added more generic NumericInput method but has no error checking.

--- GetInput.java
+++ GetInput.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;  // Import the Scanner class
 
 public class GetInput {
+
     public static int numericInput(){
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Please select a thread to choose.");
-    
-        int thread = scanner.nextInt();  
-        return thread;
+        Scanner scanner = new Scanner(System.in);  // Create a Scanner object    
+        int numericInput = scanner.nextInt(); 
+        return numericInput;     
+    }
+    public static int playAgain(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Want to play again? Enter 1 to play again and 2 to quit.");
+        int playAgainInput = scanner.nextInt();
+        return playAgainInput;
     }
 }
