dd4dfe7670d993ec0b5dc43b7c45962fceb3fdf0
Hunter <98huntermoore@gmail.com>
2020-11-14 20:23:22 -0500

    Updated menu

--- PlayGames.java
+++ PlayGames.java
@@ -1,19 +1,45 @@
 import java.util.Scanner;  // Import the Scanner class
 
 public class PlayGames {
+    private static boolean playAgain = true;
     public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Enter the game you want to play");
-    
-        String gameChosen = scanner.nextLine();  // Read user input
-        if(gameChosen.equals("1")){
-            FindTheRedThread.playGame(); 
+        System.out.println("Welcome to the Game of Games! Please enter the number of the corresponding game you wish to play:\n 1 - Find the red thread;\n 2 - Guess the number;\n 3 - Find the thimble;\n 4 - Even or Odd;\n 5 - Coin flip");
+        System.out.println("\n");
+
+        while(playAgain == true){
+            Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        
+            String gameChosen = scanner.nextLine();  // Read user input
+            if(gameChosen.equals("1")){
+                FindTheRedThread.playGame(); 
+            }
+            if(gameChosen.equals("4")){
+                EvenOdd.rules();
+                EvenOdd.PlayGame();
+            }
+
+            Scoreboard.printScoreboard();
+            String input = scanner.nextLine();
+            if(input.equals("1")){
+                playAgain = true;
+            }
+            if(input.equals("2")){
+                playAgain = false;
+            }
+            scanner.close();
         }
-        if(gameChosen.equals("2")){
-            EvenOdd.rules();
-            EvenOdd.PlayGame();
+        if(Scoreboard.userTotalScore > Scoreboard.computerTotalScore){
+            System.out.println("Congrats! You win!");
         }
+        else if(Scoreboard.userTotalScore < Scoreboard.computerTotalScore){
+            System.out.println("Sorry! The computer wins! Try again soon!");
+
+        }
+        else{
+            System.out.println("It was a tie!");
+        }
+        System.out.println("You scored " + Scoreboard.userTotalScore + " points and the computer scored " + Scoreboard.computerTotalScore + " points!");
+        System.out.println("Here's the final scoreboard!");
         Scoreboard.printScoreboard();
-        scanner.close();
     }
 }
